1cf01d357cf8eddfe9b2e035cb78021cd23b11f8
kaa-dan <nithinraj.dev@gmail.com>
2025-01-17 16:47:55 +0530

    connected to db

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,16 @@ import { setupSwagger } from './interface/swagger/swagger';
 import { logger } from './infrastructure/logger/logger';
 import { authRoutes } from './interface/routes/AuthRoute';
 import { productRoutes } from './interface/routes/ProductRoute';
+import dotenv from "dotenv"
 
-
+dotenv.config()
 
 const app = express();
 app.use(express.json());
 
 setupSwagger(app);
 
-
+const PORT = process.env.PORT||300
 // Auth routes
 app.use('/api/auth',authRoutes)
 // Product routes (protected)
@@ -20,10 +21,10 @@ app.use('/api/products',productRoutes)
 
 
 // Connect to MongoDB and start server
-mongoose.connect('mongodb://localhost:27017/clean-arch-demo')
+mongoose.connect(process.env.DB_URL as string)
   .then(() => {
     app.listen(3000, () => {
-      logger.info( `server is running on port`)
+      logger.info( `server is running on port${PORT}`)
     });
   })
   .catch(console.error);

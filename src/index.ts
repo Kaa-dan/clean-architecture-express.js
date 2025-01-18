470f66e3cdb6357bd63fe04579e2aeb9be5831c1
kaa-dan <nithinraj.dev@gmail.com>
2025-01-18 10:18:14 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,25 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const app = express();
+
 app.use(express.json());
 
 setupSwagger(app);
 
-const PORT = process.env.PORT||300
+const PORT = 3000
+
 // Auth routes
 app.use('/api/auth',authRoutes)
+
 // Product routes (protected)
-app.use('/api/products',productRoutes)
+// app.use('/api/products',productRoutes)
 
 
 // Connect to MongoDB and start server
 mongoose.connect(process.env.DB_URL as string)
   .then(() => {
     app.listen(3000, () => {
-      logger.info( `server is running on port${PORT}`)
+      logger.info( `server is running on port ${PORT}`)
     });
   })
   .catch(console.error);
--- src/infrastructure/models/UserModel.ts
+++ src/infrastructure/models/UserModel.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUserDocument extends Document {
+  _id: mongoose.Types.ObjectId;
   email: string;
   password: string;
   name: string;
